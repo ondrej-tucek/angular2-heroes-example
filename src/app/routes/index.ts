f3d902ad80c368f5739f28e85eb58fb41fcafef8
Ondrej Tucek <ondrej.tucek@gmail.com>
2016-08-13 11:18:15 +0200

    friends component added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,13 +2,15 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import { HomeViewComponent } from '../views/home';
 import { DashboardViewComponent } from '../views/dashboard';
+import { FriendsViewComponent } from '../views/friends';
 // import { AboutViewComponent } from '../views/about';
 
 
 export const routes: RouterConfig = [
     {path: '', component: HomeViewComponent},
     {path: 'home', component: HomeViewComponent},
-    {path: 'dashboard', component: DashboardViewComponent}
+    {path: 'dashboard', component: DashboardViewComponent},
+    {path: 'friends', component: FriendsViewComponent}
     // {path: 'about', component: AboutViewComponent},
     // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
     // { path: 'about', loader: () => require('es6-promise!../views/about')('AboutViewComponent')
